3e3a67799484aaba069fd19084dcc471d519786d
Javier <javi_cero@hotmail.com>
2016-09-16 03:35:13 -0500

    Se hizo Objetos 6

---
+++ src/clases/Cafetera.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+import java.awt.Event;
+
+/**
+ *
+ * @author Javier Tareas
+ */
+public class Cafetera {
+    
+    private int capacidadMaxima;
+    private int CantidadActual;
+    
+    public Cafetera(int capacidadMaxima, int CantidadActual) {
+        
+        this.capacidadMaxima = capacidadMaxima;
+        this.CantidadActual = CantidadActual;
+        
+    }
+    
+    public Cafetera() {
+        this.capacidadMaxima = 1000;
+        this.CantidadActual = 0;
+    }
+    
+    public int getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+    
+    public void setCapacidadMaxima(int capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+    }
+    
+    public int getCantidadActual() {
+        return CantidadActual;
+    }
+    
+    public void setCantidadActual(int CantidadActual) {
+        this.CantidadActual = CantidadActual;
+    }
+    
+    public void Ajustar(int aux) {
+        
+        if (this.getCantidadActual() > this.getCapacidadMaxima()) {
+            aux = this.getCapacidadMaxima() + (this.getCantidadActual() - this.getCapacidadMaxima());            
+            this.setCapacidadMaxima(aux);
+        }
+    }
+    
+    public void Llenar(int aux) {
+        if (this.getCantidadActual() < this.getCapacidadMaxima()) {
+            aux = this.getCapacidadMaxima();
+            this.setCantidadActual(aux);
+        }
+    }
+
+    public Cafetera ServirTaza(Cafetera f2) {
+        Cafetera c;
+        int CapacidadTaza, CantidadActualCafe, aux;
+        CapacidadTaza = f2.getCapacidadMaxima();
+        
+        aux = this.getCantidadActual() - f2.getCantidadActual();
+        this.setCantidadActual(aux);
+        
+        CantidadActualCafe = f2.getCantidadActual();
+               
+        c = new Cafetera(CapacidadTaza,CantidadActualCafe);
+        return c;
+    }
+    
+    public void Vaciar(int aux) {
+        aux = this.getCantidadActual() - this.getCantidadActual();
+        this.setCantidadActual(aux);
+    }
+    public String Mostrar(){
+    String aux;
+    aux = "Capacidad Maxima: " + this.getCapacidadMaxima() + "\n" 
+        + "Cantidad Actual de Café: " + this.getCantidadActual();
+
+    return aux;
+
+
+    }
+}
